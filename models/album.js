e2eebd719d2257cb151274fd64cefa6a97e2e7e3
mollywhitnack <mollywhitnack@gmail.com>
2016-07-04 12:26:45 -0700

    cleaned

--- models/album.js
+++ models/album.js
@@ -27,27 +27,6 @@ albumSchema.methods.addPictureMethod = function(picId, cb){
 }
 
 
-//use: let totalRent = album.getPropertyIncome
-/*albumSchema.methods.getPropertyIncome = function(albumId, cd){
-    let tenants = this.pictures.length;
-    var totalRent = tenants * this.rent;
-    return totalRent;
-}*/
-
-//total rent for all properties
-// use apartment.allPropertyIncomes((err, totalRent)=>{})
-//get all partments  get property income for each
-//will be asyncronous
-/*albumSchema.statics.allPropertyIncomes = function(cb){
-  this.find({}, (err, albums)=>{
-    if(err) return cb(err);
-    let totalRent = albums.reduce((total, album)=>{
-      return total + album.getPropertyIncome();
-    }, 0);
-    cb(null, totalRent);
-  });
-}*/
-
 Album =  mongoose.model('Album', albumSchema);
 
 module.exports = Album;
